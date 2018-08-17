8a0067b7ae76c0db82e2ca50e0c9a63a177b2ccc
ekultails <lshort@redhat.com>
2018-08-16 20:13:45 -0400

    Initial logic to handle batch conversions.

--- tcgc.py
+++ tcgc.py
@@ -5,6 +5,9 @@ from PIL import Image
 from math import ceil
 import logging
 import subprocess
+from os.path import basename, exists, isdir
+from os import listdir, makedirs
+from sys import exit
 
 class TCGC:
 
@@ -12,8 +15,25 @@ class TCGC:
         logging.basicConfig(level="DEBUG")
         self.height_physical_inches = 2.5
         self.width_physical_inches = 3.5
+        self.tmp_dir = "/tmp/tcgc"
+        self.tmp_dir_individuals = self.tmp_dir + "/individuals"
+        self.tmp_dir_horizontal = self.tmp_dir + "/horizontal"
+        self.tmp_dir_vertical = self.tmp_dir + "/vertical"
+
+        if not exists(self.tmp_dir):
+            makedirs(self.tmp_dir)
+            makedirs(self.tmp_dir + "/individuals")
+            makedirs(self.tmp_dir + "/horizontal")
+            makedirs(self.tmp_dir + "/vertical")
+
+    def find_first_image(self, images_dir):
+        first_image_name = listdir(images_dir)[0]
+        first_image = images_dir + "/" + first_image_name
+        logging.debug("First image found: %s" % first_image)
+        return first_image
 
     def img_info(self, image_path):
+
         with Image.open(image_path) as image:
             height, width = image.size
         return height, width
@@ -41,16 +61,60 @@ class TCGC:
         return stdout
 
     def convert_img(self, image_path, ppi):
-        convert_cmd_args = ['-units', 'PixelsPerInch', '-density', ppi, image_path, image_path + "NEWPPI"]
+        card_file_name = basename(image_path)
+        convert_cmd_args = ['-units', 'PixelsPerInch', '-density', ppi, image_path, self.tmp_dir + "/individuals/" + card_file_name]
+        self.convert(convert_cmd_args)
+        return True
+
+    def convert_merge(self, convert_merge_method, image_paths):
+
+        if convert_merge_method == "vertical":
+            convert_merge_arg = "-append"
+        elif convert_merge_method == "horizontal":
+            convert_merge_arg = "+append"
+        else:
+            logging.error("Incorrect convert_merge_method specificed." +
+                          " Please use horizontal or vertical.")
+            exit(1)
+
+        convert_cmd_args = [convert_merge_arg, *image_paths, self.tmp_dir + "/" + convert_merge_method + "/" + "out.jpg"]
         self.convert(convert_cmd_args) 
         return True
 
-    def convert_batch(self):
-        return
+    def convert_batch_individuals(self, images_dir):
 
+        first_image = self.find_first_image(images_dir)
+        first_img_info = self.img_info(first_image)
+        ppi = self.calc_ppi(first_img_info)
+
+        for image in listdir(images_dir):
+            image_path = images_dir + "/" + image
+
+            if not isdir(image_path):
+                logging.debug("Convert batch processing the image: %s" % image)
+                self.convert_img(image_path, ppi)
+
+        return True
+
+    def convert_batch_append(self):
+        image_count = 0
+        image_paths = []
+
+        for image in listdir(self.tmp_dir_individuals):
+            image_count += 1
+
+            if image_count > 4:
+                self.convert_merge("vertical", image_paths)
+                # Reset the count and paths if 4 cards have processed already
+                image_count = 0
+                image_paths = []
+            elif image_count <= 4:
+                image_path = images_dir + "/" + image
+                image_paths.append(image_path)
+
+        return True
 
 tcgc = TCGC()
-image_path = "/home/ekultails/Downloads/55-snowtrooperofficer.jpg"
-image_dimensions = tcgc.img_info(image_path)
-ppi = tcgc.calc_ppi(image_dimensions)
-tcgc.convert_img(image_path, ppi)
+images_dir = "/tmp/cards"
+tcgc.convert_batch_individuals(images_dir)
+tcgc.convert_batch_append()
