b9485ff8f85e5b9f1caac4c2020de44ee94c16fd
ekultails <ekultails@gmail.com>
2018-08-25 13:19:59 -0400

    Standardize method and variable names.

--- tcgc.py
+++ tcgc.py
@@ -9,6 +9,7 @@ from os.path import basename, exists, isdir
 from os import listdir, makedirs
 from sys import exit
 
+
 class TCGC:
 
     def __init__(self):
@@ -16,15 +17,15 @@ class TCGC:
         self.height_physical_inches = 2.5
         self.width_physical_inches = 3.5
         self.tmp_dir = "/tmp/tcgc"
-        self.tmp_dir_individuals = self.tmp_dir + "/individuals"
+        self.tmp_dir_individual = self.tmp_dir + "/individual"
         self.tmp_dir_horizontal = self.tmp_dir + "/horizontal"
         self.tmp_dir_vertical = self.tmp_dir + "/vertical"
 
         if not exists(self.tmp_dir):
             makedirs(self.tmp_dir)
-            makedirs(self.tmp_dir + "/individuals")
-            makedirs(self.tmp_dir + "/horizontal")
-            makedirs(self.tmp_dir + "/vertical")
+            makedirs(self.tmp_dir_individual)
+            makedirs(self.tmp_dir_horizontal)
+            makedirs(self.tmp_dir_vertical)
 
     def find_first_image(self, images_dir):
         first_image_name = listdir(images_dir)[0]
@@ -32,10 +33,11 @@ class TCGC:
         logging.debug("First image found: %s" % first_image)
         return first_image
 
-    def img_info(self, image_path):
+    def image_info(self, image_path):
 
         with Image.open(image_path) as image:
             height, width = image.size
+
         return height, width
 
     def calc_ppi(self, image_dimensions):
@@ -51,9 +53,9 @@ class TCGC:
         self.convert(convert_cmd_args)
         return True
 
-    def img_rotate(self, image_path):
+    def image_rotate(self, image_path):
 
-        height, width = self.img_info(image_path)
+        height, width = self.image_info(image_path)
 
         if width > height:
             logging.debug("Rotating image: %s" % image_path)
@@ -75,13 +77,13 @@ class TCGC:
         logging.debug("convert command stderr: %s" % stderr)
         return stdout
 
-    def convert_img(self, image_path_src, image_path_dest, ppi):
-        card_file_name = basename(image_path_src)
+    def convert_image_density(self, image_path_src, image_path_dest, ppi):
         convert_cmd_args = ['-units', 'PixelsPerInch', '-density', ppi, image_path_src, image_path_dest]
         self.convert(convert_cmd_args)
         return True
 
     def convert_merge(self, convert_merge_method, image_paths, merged_image_name="out.jpg"):
+        convert_merge_arg = ""
 
         if convert_merge_method == "vertical":
             convert_merge_arg = "-append"
@@ -96,11 +98,10 @@ class TCGC:
         self.convert(convert_cmd_args) 
         return True
 
-    def convert_batch_individuals(self, images_dir):
-
+    def convert_batch_individual(self, images_dir):
         first_image = self.find_first_image(images_dir)
-        first_img_info = self.img_info(first_image)
-        ppi = self.calc_ppi(first_img_info)
+        first_image_info = self.image_info(first_image)
+        ppi = self.calc_ppi(first_image_info)
 
         for image in listdir(images_dir):
             image_path_src = images_dir + "/" + image
@@ -108,17 +109,17 @@ class TCGC:
             if not isdir(image_path_src):
                 logging.debug("Convert batch processing the image: %s" % image)
                 card_file_name = basename(image_path_src)
-                image_path_dest = self.tmp_dir_individuals + "/" + card_file_name
-                self.convert_img(image_path_src, image_path_dest, ppi)
-                self.img_rotate(image_path_dest)
+                image_path_dest = self.tmp_dir_individual + "/" + card_file_name
+                self.convert_image_density(image_path_src, image_path_dest, ppi)
+                self.image_rotate(image_path_dest)
 
         return True
 
-    def convert_batch_append(self):
+    def convert_batch_append_all(self):
         total_count = 0
         image_count = 0
         image_paths = []
-        images = listdir(self.tmp_dir_individuals)
+        images = listdir(self.tmp_dir_individual)
         number_of_images = len(images)
 
         for image in images:
@@ -132,7 +133,7 @@ class TCGC:
                 image_count = 0
                 image_paths = []
             elif image_count <= 4:
-                image_path = self.tmp_dir_individuals + "/" + image
+                image_path = self.tmp_dir_individual + "/" + image
                 image_paths.append(image_path)
 
             if total_count == number_of_images:
@@ -155,7 +156,8 @@ class TCGC:
 
         return True
 
+
 tcgc = TCGC()
 images_dir = "/tmp/cards"
-tcgc.convert_batch_individuals(images_dir)
-tcgc.convert_batch_append()
+tcgc.convert_batch_individual(images_dir)
+tcgc.convert_batch_append_all()
